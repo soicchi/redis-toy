c2c25c7159ca9e226c8e4f3fe4d4587962fc1da0
soicchi <soicchi@icloud.com>
2023-10-17 23:38:46 +0900

    temp

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM python:3.12 as builder
+FROM --platform=linux/amd64 python:3.11.6 as builder
 WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-FROM python:3.12.0-slim-bullseye as runtime
+FROM --platform=linux/amd64 python:3.11.6-slim as runtime
 WORKDIR /app
-COPY --from=builder /usr/local/lib/python3.12/site-packages /usr/local/lib/python3.12/site-packages
+COPY --from=builder /usr/local/lib/python3.11/site-packages /usr/local/lib/python3.11/site-packages
 COPY --from=builder /usr/local/bin /usr/local/bin
 COPY . .
 EXPOSE 3000
-CMD ["python3", "main.py"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,11 +1,101 @@
-from fastapi import FastAPI
-from starlette.responsees import HTMLResponse
+from fastapi import FastAPI, Depends
+from redis import asyncio as aioredis
+from starlette.responses import HTMLResponse
 from starlette.websockets import WebSocket
 from starlette.staticfiles import StaticFiles
 import asyncio
 import uvloop
 import uvicorn
-import aioredis
 import datetime
+import os
+import logging
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+redis = aioredis.from_url(f"redis://{os.getenv('REDIS_HOST')}", encoding="utf-8", decode_responses=True)
+
+class Constants:
+    APP_HOST = os.getenv("APP_HOST")
+    APP_PORT = int(os.getenv("APP_PORT"))
+    STREAM_MAX_LEN = int(os.getenv("STREAM_MAX_LEN"))
+
+async def read_message(websocket: WebSocket, join_info: dict):
+    connected = True
+    is_first = True
+    stream_id = "$"
+
+    while connected:
+        try:
+            count = 1 if is_first else 100
+            results = await redis.xread(
+                streams={join_info["room"]: stream_id},
+                count=count,
+                block=100000,
+            )
+
+            for room, events in results:
+                if join_info["room"] != room:
+                    continue
+                
+                for event_id, event in events:
+                    now = datetime.datetime.now()
+
+                    await websocket.send_text(f"{now.strftime('%H:%M:%S')} {event[b'message']}")
+
+                    stream_id = event_id
+
+                if is_first:
+                    is_first = False
+        except Exception as e:
+            logging(e)
+            await redis.aclose()
+            connected = False
 
 
+async def write_message(websocket: WebSocket, join_info: dict):
+    await notify(join_info, "joined")
+
+    connected = True
+    while connected:
+        try:
+            data = await websocket.receive_text()
+            await redis.xadd(
+                join_info["room"],
+                {
+                    "username": join_info["username"],
+                    "message": data
+                },
+                id=b"*",
+                maxlen=Constants.STREAM_MAX_LEN,
+            )
+        except Exception as e:
+            logging(e)
+            await notify(join_info, "left")
+            await redis.aclose()
+            connected = False
+
+
+async def notify(join_info: dict, action: str):
+    await redis.xadd(
+        join_info["room"],
+        {"message": f"{join_info['username']} has {action}"},
+        id=b"*",
+        maxlen=Constants.STREAM_MAX_LEN,
+    )
+
+
+async def get_join_info(username: str = None, room: str = None):
+    return {"username": username, "room": room}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket, join_info: dict = Depends(get_join_info)):
+    await websocket.accept()
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    await asyncio.gather(write_message(websocket, join_info), read_message(websocket, join_info))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=Constants.APP_HOST, port=Constants.APP_PORT)
